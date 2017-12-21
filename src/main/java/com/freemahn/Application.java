a6ff0385529a68157fad6178556ae3f09739f986
Freemahn <p.gordon@interanw.ru>
2017-12-21 20:45:58 +0300

    New Hope: Add docker

--- src/main/java/com/freemahn/Application.java
+++ src/main/java/com/freemahn/Application.java
@@ -5,12 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableJpaRepositories
 @SpringBootApplication
+@RestController
 public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
-
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
 
 
     public static void main(String[] args) {
